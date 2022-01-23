36c2368f9376e8588fe7530332fe08caa0f2cadd
achraf <achrafharimza77@gmail.com>
2022-01-23 17:06:37 +0100

    depl

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -5,10 +5,14 @@ import java.sql.SQLException;
 
 public class Config {
 
-    //    private static String url = "jdbc:postgresql://localhost:5432/DBCoding";
-    private static String url = "jdbc:postgresql://localhost:5432/dbCODING";
-    private static String user = "postgres" ;
-    private static String passwd = "admin";
+
+//    private static String url = "jdbc:postgresql://localhost:5432/dbCODING";
+//    private static String user = "postgres" ;
+//    private static String passwd = "admin";
+//    private static Connection connect;
+    private static String url = "jdbc:postgresql://ec2-54-220-243-77.eu-west-1.compute.amazonaws.com:5432/ddbv1ojm3qt9br";
+    private static String user = "wcirbhlncfduts" ;
+    private static String passwd = "6bd3ce641b95c015b2fcd6ad6e0860abb78f0f16ba9a52d82365dfc3702b05ec";
     private static Connection connect;
 
     public static Connection getInstance(){
